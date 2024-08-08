419689213afef40e8ea0a23bfa3ea847fc84788a
LeThiTuongOanh <134386170+LeThiTuongOanh@users.noreply.github.com>
2024-08-08 13:07:09 +0700

    Slide69_cau6_1: Sap xep tang dan

--- Chuong2/Silde69.cpp
+++ Chuong2/Silde69.cpp
@@ -96,6 +96,22 @@ void xuatNghichDao(PhanSo ds[], int n) {
 	}
 	printf("\n");
 }
+
+void swap(PhanSo* a, PhanSo* b) {
+	PhanSo temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void sapXepTangDan(PhanSo ds[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++) {
+			if (giaTri(ds[i]) > giaTri(ds[j])) {
+				swap(&ds[i], &ds[j]);
+			}
+		}
+	}
+}
 int main() {
 	int n;
 	PhanSo ds[50];
@@ -166,7 +182,11 @@ int main() {
 			printf("Danh sach nghich dao cac phan so: ");
 			xuatNghichDao(ds, n);
 			break;
-
+		case 8:
+			sapXepTangDan(ds, n);
+			printf("Danh sach phan so sau khi sap xep tang dan: ");
+			xuatDanhSachPhanSo(ds, n);
+			break;
 	
 		case 0:
 			printf("Thoat chuong trinh.\n");
