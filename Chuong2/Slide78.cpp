aa4861a6808f918e166eb05e3d8210b8c4ca7473
Tran Chi Cong <tranchicong.dev@gmail.com>
2024-08-08 22:24:03 +0700

    Add Slide 78 Cau 2 Liet Ke cac so hoan thien

--- Chuong2/Slide78.cpp
+++ Chuong2/Slide78.cpp
@@ -41,6 +41,36 @@ void menu() {
 	printf("7. Dem so luong cac phan tu la so chan, so le, so am, so duong, so nguyen to\n");
 	printf("8. Thoat\n");
 }
+// Hàm kiểm tra số hoàn thiện
+bool laSoHoanThien(int num) {
+	if (num <= 0) return false;
+	int tong = 0;
+	for (int i = 1; i <= num / 2; i++) {
+		if (num % i == 0) {
+			tong += i;
+		}
+	}
+	return tong == num;
+}
+
+// Hàm liệt kê các số hoàn thiện trong ma trận
+void lietKeSoHoanThien(int matrix[][MAX_COL], int m, int n) {
+	bool timThay = false;
+	printf("Cac so hoan thien trong ma tran:\n");
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			if (laSoHoanThien(matrix[i][j])) {
+				printf("%d ", matrix[i][j]);
+				timThay = true;
+			}
+		}
+	}
+	if (!timThay) {
+		printf("Khong co so hoan thien nao.\n");
+	}
+	printf("\n");
+}
+
 int main() {
 	int matrix[MAX_ROW][MAX_COL];
 	
@@ -50,7 +80,7 @@ int main() {
 	// Nhập các phần tử của ma trận
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			matrix[i][j]= rand()%100;
+			matrix[i][j]= (rand()-199)%10+1;
 		}
 	}
 	for (int i = 0; i < 3; i++) {
@@ -70,7 +100,10 @@ int main() {
 			printf("Tong cac phan tu co chu so dau la chu so le: %d\n", tongChuSoDauLe(matrix, 3, 3));
 			break;
 
-		
+		case 2:
+			lietKeSoHoanThien(matrix, 3, 3);
+			break;
+
 		default:
 			printf("Lua chon khong hop le!\n");
 			break;
