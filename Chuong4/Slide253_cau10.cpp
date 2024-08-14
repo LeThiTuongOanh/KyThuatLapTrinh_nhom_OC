0248a7c21ef43f916218717df328c140f9247a51
LeThiTuongOanh <134386170+LeThiTuongOanh@users.noreply.github.com>
2024-08-14 13:36:58 +0700

    Chuong4_Slide253_cau10: Tim x theo thuat toan binarySeach de quy

---
+++ Chuong4/Slide253_cau10.cpp
@@ -0,0 +1,64 @@
+﻿#include <stdio.h>
+#include<stdlib.h>
+
+// Đệ quy: Tim kiem nhi phan tren mang so nguyen
+int binarySearch(int arr[], int left, int right, int x);
+
+int main() {
+	int n, x;
+
+	printf("Nhap so luong phan tu cua mang: ");
+	scanf_s("%d", &n);
+
+	int* arr = (int*)malloc(n * sizeof(int));
+	if (arr == NULL) {
+		printf("Khong the cap phat bo nho!\n");
+		return 1;
+	}
+
+
+	printf("Nhap cac phan tu cua mang (da sap xep):\n");
+	for (int i = 0; i < n; i++) {
+		scanf_s("%d", &arr[i]);
+	}
+
+
+	printf("Nhap gia tri x can tim: ");
+	scanf_s("%d", &x);
+
+
+	int result = binarySearch(arr, 0, n - 1, x);
+
+	// In ket qua
+	if (result != -1) {
+		printf("Gia tri %d duoc tim thay tai chi so %d.\n", x, result);
+	}
+	else {
+		printf("Gia tri %d khong ton tai trong mang.\n", x);
+	}
+	getchar();
+	getchar();
+	return 0;
+	
+}
+
+// Hàm đệ quy tìm kiếm nhị phân
+int binarySearch(int arr[], int left, int right, int x) {
+	if (left > right) {
+		return -1;
+	}
+
+	int mid = left + (right - left) / 2;
+
+	if (arr[mid] == x) {
+		return mid;
+	}
+
+	else if (x < arr[mid]) {
+		return binarySearch(arr, left, mid - 1, x);
+	}
+
+	else {
+		return binarySearch(arr, mid + 1, right, x);
+	}
+}
